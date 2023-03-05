aeef4f1738909bfec07346543123a7a4fe60883b
pablo rojas <pablorojas654@gmail.com>
2023-03-04 23:56:27 -0300

    ejr 13

---
+++ js13.js
@@ -0,0 +1,13 @@
+var persona={
+    nombre:'Pablo',
+    edad:30,
+    sexo:'hombre',
+    peso:62,
+    altura:178
+}
+
+document.write(`nombre ${persona.nombre} 
+edad ${persona.edad}
+sexo ${persona.sexo}
+peso ${persona.peso}
+altura ${persona.altura}`)
